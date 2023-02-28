8f1f0cdbb3a290a3758606fbc7683227528f6037
Elena Tykhonova <sivak_elena@ukr.net>
2023-02-28 22:42:24 +0200

    fixed bugs

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
 let birthYear = prompt('Введіть рік народження:');
-if (birthYear == null) {
+if (birthYear == null) { 
     alert('Шкода, що Ви не захотіли ввести свою дату народження');
-} 
+}
 
-let city = prompt('Введіть місто проживання:');
+let city = prompt('Введіть місто проживання:')?.trim().toLowerCase();
 if (city == null) {
     alert('Шкода, що Ви не захотіли ввести своє місто');
 }
 
-let sport = prompt('Введіть улюблений вид спорту:');
+let sport = prompt('Введіть улюблений вид спорту:')?.trim().toLowerCase();
 if (sport == null) {
     alert('Шкода, що Ви не захотіли ввести свій вид спорту');
 }
 
-let age = !birthYear ? 'Вік: інформація не надана' : `Вік: ${2023 - birthYear}`;
+let age;
+birthYear = parseInt(birthYear);
+if (birthYear > 1900 && birthYear < 2023) {
+    age = `Вік: ${2023 - birthYear}`;
+} else {
+    age = 'Вік: інформація не надана';
+}
 
 let capital;
-if (city == 'Київ') {
+if (city == 'київ') {
     capital = 'Ти живеш у столиці України';
-} else if (city == 'Вашингтон') {
+} else if (city == 'вашингтон') {
     capital = 'Ти живеш у столиці США';
-} else if (city == 'Лондон') {
+} else if (city == 'лондон') {
     capital = 'Ти живеш у слолиці Великобританії';
 } else if ((city == '') || (city == null)) {
     capital = 'Місто: інформація не надана';
@@ -30,15 +36,18 @@ else {
 } 
 
 let sportName;
-switch (sport) {
-    case 'футбол': sportName = 'Круто! Хочеш стати Андрієм Шевченком?'; break;
-    case 'легка атлетика': sportName = 'Круто! Хочеш стати Сергієм Бубкою?'; break;
-    case 'плавання': sportName = 'Круто! Хочеш стати Яною Клочковою?'; break;
-    case '': 
-    case null:
-        sportName = 'Спорт: інформація не надана.'; break;
-    default: sportName = 'Круто!'; 
+if (sport == 'футбол') {
+    sportName = 'Круто! Хочеш стати Андрієм Шевченком?';
+} else if (sport == 'легка атлетика') {
+    sportName = 'Круто! Хочеш стати Сергієм Бубкою?';
+} else if (sport == 'плавання') {
+    sportName = 'Круто! Хочеш стати Яною Клочковою?';
+} else if ((sport == '') || (sport == null)) {
+    sportName = 'Спорт: інформація не надана.';
 }
+else {
+    sportName = 'Круто!';
+} 
 
 alert(`\u2022 ${age};
 \u2022 ${capital};
